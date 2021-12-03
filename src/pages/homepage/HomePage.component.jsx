9afe66e0cf93c3871414ddc1a51ce2d659c77268
Mauricio Collis <79931370+mauriciotxt@users.noreply.github.com>
2021-12-03 13:54:14 +0000

    organize file structure, break down components to smaller parts and add image scaling css

---
+++ src/pages/homepage/HomePage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HomePage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Home = () => (
+    <div className='homepage'>
+      <Directory />
+    </div>
+)
+
+export default Home;
